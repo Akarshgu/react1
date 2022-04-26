7a963e446ba6c96f373a8e9c72423fbdf7611e85
Akarshgu <akarshgupta705415@gmail.com>
2022-04-26 19:49:20 +0530

    first commit

---
+++ react-starter/src/App.js
@@ -0,0 +1,16 @@
+const App=()=>{
+  return (
+    <div className="root">
+    <h1>Vertual DOM</h1>
+    <p>DOM is a document object model, created by converting 
+      HTML CSS and JS Real DOM, which is an object which gets
+       created whenever any React application gets loaded on
+        the screen for the first time., whenever React 
+        components gets mounted on the screen for the first 
+        time. 
+              </p>
+    <button>please click me</button>
+    </div>
+  )
+}
+export default App;
